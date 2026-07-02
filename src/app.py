e4ef1ddf93df446a9b6cfee1926068f060552b5a
Paras <111403754+paras9o9@users.noreply.github.com>
2026-07-02 10:46:13 +0530

    up

--- src/app.py
+++ src/app.py
@@ -315,7 +315,7 @@ div[data-testid="stExpander"] summary {
     transition: background 0.12s !important;
 }
 
-div[data-testid="stExpander"] summary:hover { background: #fafafa !important;}
+div[data-testid="stExpander"] summary:hover { background: #fafafa !important; color: black;}
 # div[data-testid="stExpander"] details > div { padding: 0 16px 16px !important; }
 
 /* ── Selectbox ── */
